7d0d5f189637f0faefe8c24bf184c9a0c4fccb61
cobendaniel <danielchoi@hotmail.co.uk>
2024-05-06 19:54:09 +0900

    URI length problems, will require the API to be changed from GET to POST

--- api_code/ImageUpscalerAPI.py
+++ api_code/ImageUpscalerAPI.py
@@ -1,6 +1,6 @@
 from flask import Flask, Jsonify, request
 
-from image_scaler.image_upscaler import image_upscaler
+from image_upscaler.image_upscaler import image_upscaler
 
 flask_image_upscaler_interface = Flask(__name__)
 
@@ -9,10 +9,13 @@ def upscale_image():
     original_name   = request.json["image_name"]
     binary_image    = request.json["image_file"]
     image_modifier  = request.json["upscale_modifier"]
-    processed_image = image_upscaler(binary_image, image_modifier)
+    processed_image = image_upscaler(original_name, binary_image, image_modifier)
     return_object   = {
         "status" : f"{original_name} converted",
         "image"  : processed_image
     }
 
     return Jsonify(return_object)
+
+if (__name__ == "__main__"):
+    flask_image_upscaler_interface.run()
--- image_upscaler/image_upscaler.py
+++ image_upscaler/image_upscaler.py
@@ -1,15 +1,22 @@
-import io
+import io, os
 
 from PIL         import Image
 from super_image import EdsrModel, ImageLoader
 
-def image_upscaler(image_file: str, upscale_modifier: int):    
+def image_upscaler(image_file_name: str, image_file: str, upscale_modifier: int):    
+    def convert_image_to_binary(image_file_name: str, upscale_modifier: int):
+        with open(f"upscaled_{upscale_modifier}_{image_file_name}", "rb") as f:
+            binary_image_string = f.read()        
+
+        os.remove(f"upscaled_{upscale_modifier}_{image_file_name}")
+
+        return binary_image_string
+
     binary_data    = io.BytesIO(image_file)
     original_image = Image.open(binary_data)
     upscaler_model = EdsrModel.from_pretrained("eugenesiow/edsr", scale = upscale_modifier)
     input_image    = ImageLoader.load_image(original_image)
-    upscaled_image = upscaler_model(input_image)
-    output_stream  = io.BytesIO()
-    upscaled_image.save(output_stream, format = "JPEG")    
+    upscaled_image = upscaler_model(input_image)    
+    ImageLoader.save_image(upscaled_image, f"upscaled_{upscale_modifier}_{image_file_name}")    
 
-    return output_stream.get_value()
+    return convert_image_to_binary(image_file_name, upscale_modifier)
