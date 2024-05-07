57dd52803924adb12013f33776375d9105b1679e
cobendaniel <78524895+cobendaniel@users.noreply.github.com>
2024-05-07 11:40:15 +0900

    Update image_upscaler.py

--- image_upscaler/image_upscaler.py
+++ image_upscaler/image_upscaler.py
@@ -3,7 +3,7 @@ import io, os
 from PIL         import Image
 from super_image import EdsrModel, ImageLoader
 
-def image_upscaler(image_file_name: str, image_file: str, upscale_modifier: int):    
+def image_upscaler(image_file_name: str, binary_image_file: str, upscale_modifier: int):    
     def convert_image_to_binary(image_file_name: str, upscale_modifier: int):
         with open(f"upscaled_{upscale_modifier}_{image_file_name}", "rb") as f:
             binary_image_string = f.read()        
@@ -12,11 +12,13 @@ def image_upscaler(image_file_name: str, image_file: str, upscale_modifier: int)
 
         return binary_image_string
 
-    binary_data    = io.BytesIO(image_file)
-    original_image = Image.open(binary_data)
+    original_image = Image.open(binary_image_file)
     upscaler_model = EdsrModel.from_pretrained("eugenesiow/edsr", scale = upscale_modifier)
     input_image    = ImageLoader.load_image(original_image)
     upscaled_image = upscaler_model(input_image)    
     ImageLoader.save_image(upscaled_image, f"upscaled_{upscale_modifier}_{image_file_name}")    
 
-    return convert_image_to_binary(image_file_name, upscale_modifier)
+    binary_image = convert_image_to_binary(image_file_name, upscale_modifier)
+    Image.open(binary_image).show()
+
+    return binary_image
